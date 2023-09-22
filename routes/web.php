37541acbcb944c70a89a33b0cecd2b729d6060f7
owenwattimena <wentoxwtt@gmail.com>
2023-09-23 00:10:35 +0900

    chore: add middleware auth:admin and guest to web route

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('templates.index');
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/', function () {
+        return view('templates.index');
+    });
 });
-Route::prefix('auth')->group(function(){
-    Route::get('login', [AuthController::class, 'login'])->name('auth.login');
-    Route::post('login', [AuthController::class, 'doLogin'])->name('auth.doLogin');
+
+Route::middleware('guest')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::get('login', [AuthController::class, 'login'])->name('auth.login');
+        Route::post('login', [AuthController::class, 'doLogin'])->name('auth.doLogin');
+    });
 });
